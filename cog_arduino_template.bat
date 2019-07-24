52c00e6c2a10142b51e82ac7a8c79ff29460080d
Nick B <nbourrereg@hotmail.com>
2019-07-24 12:14:10 -0400

    Modified the bat file to accept arguments

--- cog_arduino_template.bat
+++ cog_arduino_template.bat
@@ -1,1 +1,12 @@
-cog -d -D prefix=Test -o TestMgr.h cog_arduino_template.h
+@echo off
+
+if "%~1"=="" goto NO_ARGS
+cog -d -D prefix=%1 -o %1Mgr.h cog_arduino_mgr_template.h
+
+goto DONE
+
+:NO_ARGS
+echo Error no output file given
+goto DONE
+
+:DONE
